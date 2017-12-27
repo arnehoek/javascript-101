66d854de3c28d0d54fcff5bf06aa466ed9938702
Arne Hoek <ahoek@ilionx.com>
2017-12-27 13:11:17 +0100

    ES6-basic classes - add cow

--- js/init.js
+++ js/init.js
@@ -1,2 +1,3 @@
 let animal = new Animal();
 let bear = new Bear();
+let cow = new Cow();
---
+++ js/model/cow.js
@@ -0,0 +1,6 @@
+class Cow extends Animal {
+    constructor() {
+        super();
+        console.log('we created a cow');
+    }
+}
